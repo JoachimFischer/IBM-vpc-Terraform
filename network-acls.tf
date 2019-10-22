67838130d9ebc6e6f3b2d9bbd3f0b490311daf43
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-10-22 10:13:22 +0200

    change inbound/outbound

--- network-acls.tf
+++ network-acls.tf
@@ -3,14 +3,14 @@ resource "ibm_is_network_acl" "default_all_acl" {
 
   rules = [
     {
-      name        = "${var.vpc-name}-default-allow-all-ingress"
+      name        = "${var.vpc-name}-default-allow-all-inbound"
       action      = "allow"
       source      = "0.0.0.0/0"
       destination = "0.0.0.0/0"
       direction   = "inbound"
     },
     {
-      name        = "${var.vpc-name}-default-allow-all-egress"
+      name        = "${var.vpc-name}-default-allow-all-outbound"
       action      = "allow"
       source      = "0.0.0.0/0"
       destination = "0.0.0.0/0"
