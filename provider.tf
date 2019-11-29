0250fd147e8514890f33cc06e5b94167d136c36f
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-11-29 12:03:28 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 provider "ibm" {
   ibmcloud_api_key        = "${var.ibmcloud_api_key}"
   ibmcloud_timeout        = "${var.ibmcloud_timeout}"
-  ibmcloud_vpc_generation = "${var.ibmcloud_vpc_generation}"
-  ibmcloud_region         = "${var.ibmcloud_region}"
+  generation              = "${var.ibmcloud_vpc_generation}"
+  region                  = "${var.ibmcloud_region}"
 }
