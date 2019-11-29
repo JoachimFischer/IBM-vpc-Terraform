59359980ed510390a5b1c059687d5f7f374f2b5b
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-11-29 11:07:08 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 # variables supplied from terraform.tfvars
 
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
-  ibmcloud_timeout   = "${var.ibmcloud_timeout}"
-  generation = 1
-  region="eu-de"
+  ibmcloud_api_key        = "${var.ibmcloud_api_key}"
+  ibmcloud_timeout        = "${var.ibmcloud_timeout}"
+  ibmcloud_vpc_generation = "${var.ibmcloud_vpc_generation}"
+  ibmcloud_region         = "${var.ibmcloud_region}"
 }
