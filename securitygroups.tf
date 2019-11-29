4f956230fc7ca023697fe9deac7503baa033889d
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-11-29 11:40:17 +0100

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,21 +1,21 @@
 #---------------------------------------------------------
 # Create Webapptier Security Group & Rules
 #---------------------------------------------------------
-resource "ibm_is_security_group" "webapptier-securitygroup" {
-  name = "${var.vpc-name}-webapptier-securitygroup"
+resource "ibm_is_security_group" "server-securitygroup" {
+  name = "${var.vpc-name}-server-securitygroup"
   vpc  = "${ibm_is_vpc.vpc1.id}"
 }
 
-resource "ibm_is_security_group_rule" "webapptier-securitygroup-rule1" {
-  group      = "${ibm_is_security_group.webapptier-securitygroup.id}"
+resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
+  group      = "${ibm_is_security_group.server-securitygroup.id}"
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
 }
 
 
-resource "ibm_is_security_group_rule" "webapptier-securitygroup-rule2" {
-  group      = "${ibm_is_security_group.webapptier-securitygroup.id}"
+resource "ibm_is_security_group_rule" "server-securitygroup-rule2" {
+  group      = "${ibm_is_security_group.server-securitygroup.id}"
   direction  = "outbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
