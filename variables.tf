28826e8824b2f18593f79300c409790bc560d15e
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-12-02 11:50:25 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -77,12 +77,42 @@ variable "ssh_keyname" {}
 
 #---------------------------------------------------------
 ## DEFINE OS image to be used for compute instances
+## The image types are different between Gen1 and Gen2
+##
+##        GEN 1                         GEN 2
+##    "windows-2016-amd64"    = "ibm-windows-2016-full-std-64"
+##    "ubuntu-16.04-amd64"    = "ibm-ubuntu-16-04-05-64-minimal-for-vsi"
+##    "windows-2012-r2-amd64" = "ibm-windows-2012-r2-full-std-64"
+##    "windows-2012-amd64"    = "ibm-windows-2012-full-std-64"
+##    "centos-7.x-amd64"      = "ibm-centos-7-0-64"
+##    "ubuntu-18.04-amd64"    = "ibm-ubuntu-18-04-64"
+##    "debian-9.x-amd64"      = "ibm-debian-9-0-64-minimal-for-vsi"
 #---------------------------------------------------------
-image = Ubuntu-16.04-amd64
-
+variable "image" {
+   default = "Ubuntu-16.04-amd64"
+}
 
 #---------------------------------------------------------
 ## DEFINE compute instance profile & quantity
+## The image types are different between Gen1 and Gen2
+##
+##        GEN 1        GEN 2
+##   "bc1-16x64"   = "bx2-16x64"
+##    "bc1-2x8"    = "bx2-2x8"
+##    "bc1-32x128" = "bx2-32x128"
+##    "bc1-48x192" = "bx2-48x192"
+##    "bc1-4x16"   = "bx2-4x16"
+##    "bc1-8x32"   = "bx2-8x32"
+##    "cc1-16x32"  = "cx2-16x32"
+##    "cc1-2x4"    = "cx2-2x4"
+##    "cc1-32x64"  = "cx2-32x64"
+##    "cc1-4x8"    = "cx2-4x8"
+##    "cc1-8x16"   = "cx2-8x16"
+##    "mc1-16x128" = "mx2-16x128"
+##    "mc1-2x16"   = "mx2-2x16"
+##    "mc1-32x256" = "mx2-32x256"
+##   "mc1-4x32"    = "mx2-4x32"
+##    "mc1-8x64"   = "mx2-8x64"
 #---------------------------------------------------------
 variable "profile-server" {
   default = "cc1-2x4"
