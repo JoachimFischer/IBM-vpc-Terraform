039be83f2963f558172e20d6004a84ea0fe3c376
Joachim Fischer <joachim.fischer@de.ibm.com>
2019-12-02 11:08:50 +0100

    Update network-acls.tf

--- network-acls.tf
+++ network-acls.tf
@@ -1,21 +1,21 @@
-resource "ibm_is_network_acl" "default_all_acl" {
-  name = "${var.vpc-name}-default-all-acl"
+#resource "ibm_is_network_acl" "default_all_acl" {
+#  name = "${var.vpc-name}-default-all-acl"
 
-  rules = [
-    {
-      name        = "${var.vpc-name}-default-allow-all-inbound"
-      action      = "allow"
-      source      = "0.0.0.0/0"
-      destination = "0.0.0.0/0"
-      direction   = "inbound"
-    },
-    {
-      name        = "${var.vpc-name}-default-allow-all-outbound"
-      action      = "allow"
-      source      = "0.0.0.0/0"
-      destination = "0.0.0.0/0"
-      direction   = "outbound"
-    },
-  ]
-}
+# rules = [
+#    {
+#      name        = "${var.vpc-name}-default-allow-all-inbound"
+#      action      = "allow"
+#      source      = "0.0.0.0/0"
+#      destination = "0.0.0.0/0"
+#      direction   = "inbound"
+#    },
+#    {
+#      name        = "${var.vpc-name}-default-allow-all-outbound"
+#      action      = "allow"
+#      source      = "0.0.0.0/0"
+#      destination = "0.0.0.0/0"
+#      direction   = "outbound"
+#    },
+#  ]
+#}
 
