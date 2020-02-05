62eb2d530f30edd7ef3a8822fa1336e601fb1536
René Meyer <rene.meyer@de.ibm.com>
2020-02-05 11:05:33 +0100

    fix tf version syntax

--- securitygroups.tf
+++ securitygroups.tf
@@ -3,11 +3,11 @@
 #---------------------------------------------------------
 resource "ibm_is_security_group" "server-securitygroup" {
   name = "${var.vpc-name}-server-securitygroup"
-  vpc  = "${ibm_is_vpc.vpc1.id}"
+  vpc  = ibm_is_vpc.vpc1.id
 }
 
 resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
-  group      = "${ibm_is_security_group.server-securitygroup.id}"
+  group      = ibm_is_security_group.server-securitygroup.id
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
@@ -15,7 +15,7 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
 
 
 resource "ibm_is_security_group_rule" "server-securitygroup-rule2" {
-  group      = "${ibm_is_security_group.server-securitygroup.id}"
+  group      = ibm_is_security_group.server-securitygroup.id
   direction  = "outbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
