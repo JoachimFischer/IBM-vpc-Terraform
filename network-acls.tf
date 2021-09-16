5c3a51f0e2498d040b358251f379a17a464630ae
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-16 17:04:29 +0200

    Update network-acls.tf

--- network-acls.tf
+++ network-acls.tf
@@ -8,23 +8,30 @@ resource "ibm_is_network_acl" "default_all_acl" {
   vpc  = ibm_is_vpc.vpc1
 }
 
-resource "ibm_is_network_acl_rule" "isExampleACLRule1" {
-      name        = "${var.vpc-name}-default-allow-all-inbound"
-      action      = "allow"
-      source      = "0.0.0.0/0"
-      destination = var.server-subnet-zone-1
-      direction   = "inbound"
-      tcp = {
+resource "ibm_is_network_acl" "is-vpc-acl" {
+  name = "is-vpc-acl-1"
+  vpc  = ibm_is_vpc.vpc1
+  rules {
+    name        = "outbound"
+    action      = "allow"
+    source      = "0.0.0.0/0"
+    destination = "0.0.0.0/0"
+    direction   = "outbound"
+    icmp {
+      code = 1
+      type = 1
+    }
+  }
+  rules {
+    name        = "inbound"
+    action      = "allow"
+    source      = "0.0.0.0/0"
+    destination = var.server-subnet-zone-1
+    direction   = "inbound"
+    tcp = {
         port_max = 22
         port_min = 22
       }
-    }
-
-resource "ibm_is_network_acl_rule" "isExampleACLRule2" {
-      name        = "${var.vpc-name}-default-allow-all-outbound"
-      action      = "allow"
-      source      = "0.0.0.0/0"
-      destination = "0.0.0.0/0"
-      direction   = "outbound"
-    }
+  }
+}
 
