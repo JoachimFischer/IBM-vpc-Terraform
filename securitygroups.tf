c64ce626e3f168f11a7a19c1a7223157a7a0a05d
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-16 17:18:03 +0200

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -11,6 +11,10 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
 }
 
 
