3a292f6654d31a1849ee7dbb00b40bc5277d0ae1
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-16 10:52:56 +0200

    Update vpn.tf

--- vpn.tf
+++ vpn.tf
@@ -1,14 +1,19 @@
-resource "ibm_is_vpn_gateway" "testacc_vpn_gateway" {
-  name   = var.vpn_gw
-  subnet = ibm_is_subnet.server-subnet-zone1.id
-}
+#--------------------------------
+#  VPNaaS definition
+#--------------------------------
+#
 
-resource "ibm_is_vpn_gateway_connection" "VPNGatewayConnection" {
-  name          =  var.vpn_gw_con
-  vpn_gateway   = ibm_is_vpn_gateway.testacc_vpn_gateway.id
-  peer_address  = var.peer_adress
-  preshared_key = var.preshared_key
-  local_cidrs = [ibm_is_subnet.server-subnet-zone1.ipv4_cidr_block]
-  peer_cidrs = var.peer_ipv4_cidr_block
-  admin_state_up = true
-}
+#resource "ibm_is_vpn_gateway" "testacc_vpn_gateway" {
+#  name   = var.vpn_gw
+#  subnet = ibm_is_subnet.server-subnet-zone1.id
+#}
+
+#resource "ibm_is_vpn_gateway_connection" "VPNGatewayConnection" {
+#  name          =  var.vpn_gw_con
+#  vpn_gateway   = ibm_is_vpn_gateway.testacc_vpn_gateway.id
+#  peer_address  = var.peer_adress
+#  preshared_key = var.preshared_key
+#  local_cidrs = [ibm_is_subnet.server-subnet-zone1.ipv4_cidr_block]
+#  peer_cidrs = var.peer_ipv4_cidr_block
+#  admin_state_up = true
+#}
