1b96638763a8d73f4b4a22bdcb46bd66b15e14d2
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-16 17:46:01 +0200

    Update network-gw.tf

--- network-gw.tf
+++ network-gw.tf
@@ -2,13 +2,12 @@
 # define network gateway
 #----------------------------------
 
-resource "ibm_is_public_gateway" "vpc1_gateway" {
-  name = "vpc1-gateway"
+#---------------------------------------------------------
+# Get Public Gateway's for Zone 1
+#---------------------------------------------------------
+resource "ibm_is_public_gateway" "pubgw-zone1" {
+  name = "${var.vpc-name}-${var.zone1}-pubgw"
   vpc  = ibm_is_vpc.vpc1.id
-  zone = var.server-subnet-zone-1
-
-  //User can configure timeouts
-  timeouts {
-    create = "90m"
-  }
+  zone = var.zone1
 }
+
