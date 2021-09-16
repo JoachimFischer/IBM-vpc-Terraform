28def1325d88c9b25e64f15343b94f319fd063d1
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-16 10:41:15 +0200

    Update network-acls.tf

--- network-acls.tf
+++ network-acls.tf
@@ -1,21 +1,27 @@
-#resource "ibm_is_network_acl" "default_all_acl" {
-#  name = "${var.vpc-name}-default-all-acl"
+## Connect Server via SSH and port 22             ##
+##
+resource "ibm_is_network_acl" "default_all_acl" {
+  name = "${var.vpc-name}-default-all-acl"
 
-# rules = [
-#    {
-#      name        = "${var.vpc-name}-default-allow-all-inbound"
-#      action      = "allow"
-#      source      = "0.0.0.0/0"
-#      destination = "0.0.0.0/0"
-#      direction   = "inbound"
-#    },
-#    {
-#      name        = "${var.vpc-name}-default-allow-all-outbound"
-#      action      = "allow"
-#      source      = "0.0.0.0/0"
-#      destination = "0.0.0.0/0"
-#      direction   = "outbound"
-#    },
-#  ]
-#}
+ rules = [
+    {
+      name        = "${var.vpc-name}-default-allow-all-inbound"
+      action      = "allow"
+      source      = "0.0.0.0/0"
+      destination = "${var.server-subnet-zone-1}"
+      direction   = "inbound"
+      tcp {
+        port_max        = 22
+        port_min        = 22
+      }   
+    },
+    {
+      name        = "${var.vpc-name}-default-allow-all-outbound"
+      action      = "allow"
+      source      = "0.0.0.0/0"
+      destination = "0.0.0.0/0"
+      direction   = "outbound"
+    },
+  ]
+}
 
