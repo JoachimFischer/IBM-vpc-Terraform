1aa17c4a92ce6c9b4039fb15ccf9f71c8055e671
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-17 16:15:02 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,13 +3,13 @@
 #  Output of IPs from public gateways
 #--------------------------------------
 output "pgw_ip_addresses" {
-  value = ibm_is_public_gateway.pubgw[*].floating_ip.address
+  value = ibm_is_public_gateway.pubgw-zone1.floating_ip.address
 }
 
 #--------------------------------------
 # Output of Floating IPs
 #--------------------------------------
 output "ip_address" {
-  value = ibm_is_floating_ip.server-zone[*]-fip.address
+  value = ibm_is_floating_ip.server-zone1-fip.address
 }
 
