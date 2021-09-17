ac201e84bff917e69e8655785028c3089f5fe961
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-17 13:25:24 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,14 @@
+#-------------------------------------------------------
 # variables supplied from terraform.tfvars
+# Please update and check necessary Terraform version. 
+# information is available at Terraform provider Doc. (TODO)
+#-------------------------------------------------------
 
 terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "~> 1.12.0"
+      version = "v1.14.0"
     }
   }
 }
