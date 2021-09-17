6fc7c6b9703dfa9636744bcbfb37c093dedb7264
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-17 12:48:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,17 +21,31 @@ resource "ibm_is_vpc" "vpc1" {
 }
 
 #---------------------------------------------------------
-# Create new address prefixes in VPC
+# Create new address prefixes in VPC Zones
 #---------------------------------------------------------
-resource "ibm_is_vpc_address_prefix" "prefix1" {
+resource "ibm_is_vpc_address_prefix" "prefix-1" {
   name = "zone1-cidr-1"
   vpc  = ibm_is_vpc.vpc1.id
   zone = var.zone1
-  cidr = var.vpc-address-prefix
+  cidr = var.vpc-address-prefix-1
+}
+
+resource "ibm_is_vpc_address_prefix" "prefix-2" {
+  name = "zone2-cidr-2"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone2
+  cidr = var.vpc-address-prefix-2
+}
+
+resource "ibm_is_vpc_address_prefix" "prefix-3" {
+  name = "zone3-cidr-3"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone3
+  cidr = var.vpc-address-prefix-2
 }
 
 #---------------------------------------------------------
-## Create Webapp Subnets in Zone1
+## Create Subnets in Zone1 / Zone2 / Zone3
 #---------------------------------------------------------
 resource "ibm_is_subnet" "server-subnet-zone1" {
   name            = "${var.vpc-name}-${var.zone1}-server"
@@ -39,4 +53,20 @@ resource "ibm_is_subnet" "server-subnet-zone1" {
   zone            = var.zone1
   ipv4_cidr_block = var.server-subnet-zone-1
   public_gateway  = ibm_is_public_gateway.pubgw-zone1.id
+}
+
+resource "ibm_is_subnet" "server-subnet-zone2" {
+  name            = "${var.vpc-name}-${var.zone2}-server"
+  vpc             = ibm_is_vpc.vpc1.id
+  zone            = var.zone2
+  ipv4_cidr_block = var.server-subnet-zone-2
+  public_gateway  = ibm_is_public_gateway.pubgw-zone2.id
+}
+
+resource "ibm_is_subnet" "server-subnet-zone3" {
+  name            = "${var.vpc-name}-${var.zone3}-server"
+  vpc             = ibm_is_vpc.vpc1.id
+  zone            = var.zone3
+  ipv4_cidr_block = var.server-subnet-zone-3
+  public_gateway  = ibm_is_public_gateway.pubgw-zone3.id
 }
