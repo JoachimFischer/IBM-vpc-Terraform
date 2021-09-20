6381240c135e3e35b5ded86cb3bda251169467e9
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-20 11:43:45 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,6 +15,5 @@ terraform {
 
 provider "ibm" {
   ibmcloud_api_key        = var.ibmcloud_api_key
-  ibmcloud_timeout        = var.ibmcloud_timeout
   region                  = var.ibmcloud_region
 }
