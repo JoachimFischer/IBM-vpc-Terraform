0312c525fa6aad00cef4bedb83faa15f288c5ef7
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-20 13:01:46 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -12,12 +12,25 @@ output "pgw_ip_addresses" {
 #--------------------------------------
 # Output of Floating IPs
 #--------------------------------------
- output "ip_address" {
+ output "ip_address_server_zone1" {
    value = ibm_is_floating_ip.server-zone1-fip.address
  }
 
 #--------------------------------------
+# Output of Subnet
+#--------------------------------------
+ output "subnet_zone1" {
+   value = ibm_is_subnet.server-subnet-zone1
+ }
+ output "subnet_zone2" {
+   value = ibm_is_subnet.server-subnet-zone2
+ }
+ output "subnet_zone3" {
+   value = ibm_is_subnet.server-subnet-zone3
+ }
 
+
+-------------------------------------------------
 #output "vpc" {
 #	value = {
 #		vpc = {
