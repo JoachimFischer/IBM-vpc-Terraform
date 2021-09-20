8870bceee3667923247657de4509d6e8bb368072
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-20 13:21:00 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -20,13 +20,13 @@ output "pgw_ip_addresses" {
 # Output of Subnet
 #--------------------------------------
  output "subnet_zone1" {
-   value = ibm_is_subnet.server-subnet-zone1
+   value = ibm_is_subnet.server-subnet-zone1.ipv4_cidr_block
  }
 # output "subnet_zone2" {
-#   value = ibm_is_subnet.server-subnet-zone2
+#   value = ibm_is_subnet.server-subnet-zone2.ipv4_cidr_block
 # }
 # output "subnet_zone3" {
-#   value = ibm_is_subnet.server-subnet-zone3
+#   value = ibm_is_subnet.server-subnet-zone3.ipv4_cidr_block
 # }
 
 
