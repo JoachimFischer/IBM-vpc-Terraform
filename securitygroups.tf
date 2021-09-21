728cb88cc6589ef5699dc08a2182a305622fff98
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-09-21 10:31:08 +0200

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -6,7 +6,7 @@ resource "ibm_is_security_group" "server-securitygroup" {
   vpc  = ibm_is_vpc.vpc1.id
 }
 
-resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
+resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp" {
   group      = ibm_is_security_group.server-securitygroup.id
   direction  = "inbound"
   ip_version = "ipv4"
@@ -15,6 +15,12 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
       port_min = 22
       port_max = 22
   }
+  
+  resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-icmp" {
+  group      = ibm_is_security_group.server-securitygroup.id
+  direction  = "inbound"
+  ip_version = "ipv4"
+  remote     = "0.0.0.0/0"
   icmp {
     code = 0
     type = 8
@@ -22,7 +28,7 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule1" {
 }
 
 
-resource "ibm_is_security_group_rule" "server-securitygroup-rule2" {
+resource "ibm_is_security_group_rule" "server-securitygroup-rule-out-all" {
   group      = ibm_is_security_group.server-securitygroup.id
   direction  = "outbound"
   ip_version = "ipv4"
